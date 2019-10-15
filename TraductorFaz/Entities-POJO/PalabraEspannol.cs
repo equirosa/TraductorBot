1ba71ef3fbc6631aa9542ea62f4c3d83ef41fcff
equirosa <eduardo@eduardoquiros.com>
2019-10-15 11:20:21 -0600

    Crear nuevo objeto para palabras en español.

---
+++ TraductorFaz/Entities-POJO/PalabraEspannol.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_POJO
+{
+    class PalabraEspannol
+    {
+        private string Palabra { get; set; }
+        private int Popularidad { get; set; }
+
+        public PalabraEspannol() { }
+        public PalabraEspannol(string[] infoArray)
+        {
+            if(infoArray != null && infoArray.Length >= 2)
+            {
+                Palabra = infoArray[0];
+                if (Int32.TryParse(infoArray[1], out int pop))
+                    Popularidad = pop;
+                else
+                    throw new Exception("La popularidad debe ser un número.");
+            }
+            else
+            {
+                throw new Exception("La información solicitada es necesaria.");
+            }
+        }
+    }
+}
