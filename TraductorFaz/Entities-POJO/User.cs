8469514bc0e8c25fa53d9d6ac4aab1aa4bf28508
equirosa <equirosa@ucenfotec.ac.cr>
2019-10-15 08:51:49 -0600

    Create user class.

---
+++ TraductorFaz/Entities-POJO/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_POJO
+{
+    public class User : BaseEntity
+    {
+        private string Id { get; set;}
+        private string Name { get; set; }
+
+        public User() { }
+        public User(string[] infoArray)
+        {
+            if(infoArray!=null && infoArray.Length >= 2)
+            {
+                Id = infoArray[0];
+                Name = infoArray[1];
+            }
+            else
+            {
+                throw new Exception("Todos los valores son requeridos. [Id,Name]");
+            }
+        }
+    }
+}
