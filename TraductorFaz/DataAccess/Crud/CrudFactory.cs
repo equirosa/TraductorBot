c33cec7653d63743b62b4421dad4dcb1829e638a
equirosa <eduardo@eduardoquiros.com>
2019-10-15 11:00:27 -0600

    Start data access section.

---
+++ TraductorFaz/DataAccess/Crud/CrudFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public abstract class CrudFactory
+    {
+        protected SqlDao dao;
+
+        public abstract void Create(BaseEntity entity);
+        public abstract T Retrieve<T>(BaseEntity entity);
+        public abstract List<T> RetrieveAll<T>();
+        public abstract void Update(BaseEntity entity);
+        public abstract void Delete(BaseEntity entity);
+
+    }
+}
