b47dacdb539271daa9768123c5822d344223b95c
equirosa <equirosa@ucenfotec.ac.cr>
2019-10-15 08:45:20 -0600

    Initial commit.

---
+++ TraductorFaz/TraductorFaz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraductorFaz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola!");
+            Console.ReadKey();
+        }
+    }
+}
