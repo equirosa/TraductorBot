3d6c031d63f3843a1b4f384397187cc26733d07c
equirosa <eduardo@eduardoquiros.com>
2019-10-15 13:27:31 -0600

    Actualizar atributos y constructores de Traduccion para emparejar con la base de datos.

--- TraductorFaz/Entities-POJO/Traduccion.cs
+++ TraductorFaz/Entities-POJO/Traduccion.cs
@@ -8,18 +8,24 @@ namespace Entities_POJO
 {
     public class Traduccion : BaseEntity
     {
-        public string Original { get; set; }
+        public int Id { get; set; }
+        public PalabraEspannol Original { get; set; }
         public Lenguaje Objetivo { get; set; }
         public string Resultado { get; set; }
+        public Usuario Creador { get; set; }
+        public DateTime FechaCreacion { get; set; }
 
         public Traduccion() { }
-        public Traduccion(string spanish, Lenguaje lenguaje, string resultado)
+        public Traduccion(int id, PalabraEspannol spanish, Lenguaje lenguaje, string resultado, Usuario creador, DateTime fechacreacion)
         {
             if(spanish != null && lenguaje != null && resultado!= null)
             {
+                Id = id;
                 Original = spanish;
                 Objetivo = lenguaje;
                 Resultado = resultado;
+                Creador = creador;
+                FechaCreacion = fechacreacion;
             }
             else
             {
