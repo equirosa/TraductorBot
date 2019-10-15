5fc961beef81e109fed1e43592232f08defabb37
equirosa <equirosa@ucenfotec.ac.cr>
2019-10-15 08:53:24 -0600

    Create language class.

---
+++ TraductorFaz/Entities-POJO/Language.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Entities_POJO
+{
+    public class Language : BaseEntity
+    {
+        private string Id { get; set; }
+        private string Name { get; set; }
+
+        public Language() { }
+        public Language(string[] infoArray)
+        {
+            if (infoArray != null && infoArray.Length >= 2)
+            {
+                Id = infoArray[0];
+                Name = infoArray[1];
+            }
+            else
+            {
+                throw new Exception("Todos los valores son requeridos. [Id,Name]");
+            }
+        }
+    }
+}
