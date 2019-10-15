f8edf90984d70ecb418bc5bf9a46887b36eb5e74
equirosa <eduardo@eduardoquiros.com>
2019-10-15 15:18:16 -0600

    Crear Managers para todas las clases.

---
+++ TraductorFaz/TraductorFaz/PalabraEspannolManager.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Crud;
+using Entities_POJO;
+using Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraductorFaz
+{
+    public class PalabraEspannolManager
+    {
+        private PalabraEspannolCrudFactory crudPalabraEspannol;
+
+        public PalabraEspannolManager()
+        {
+            crudPalabraEspannol = new PalabraEspannolCrudFactory();
+        }
+
+        public void Create(PalabraEspannol palabraEspannol)
+        {
+            try
+            {
+            var c = crudPalabraEspannol.Retrieve<PalabraEspannol>(palabraEspannol);
+
+            if (c != null)
+            {
+                //PalabraEspannol already exists.
+                throw new BussinessException(3);
+            }
+            crudPalabraEspannol.Create(palabraEspannol);
+        }
+        catch (Exception ex)
+        {
+            ExceptionManager.GetInstance().Process(ex);
+            }
+    }
+
+        public List<PalabraEspannol> RetrieveAll()
+    {
+        return crudPalabraEspannol.RetrieveAll<PalabraEspannol>();
+    }
+
+    public PalabraEspannol RetrieveById(PalabraEspannol palabraEspannol)
+    {
+        return crudPalabraEspannol.Retrieve<PalabraEspannol>(palabraEspannol);
+    }
+
+    internal void Update(PalabraEspannol palabraEspannol)
+    {
+        crudPalabraEspannol.Update(palabraEspannol);
+    }
+
+    internal void Delete(PalabraEspannol palabraEspannol)
+    {
+            crudPalabraEspannol.Delete(palabraEspannol);
+    }
+    }
+}
---
+++ TraductorFaz/TraductorFaz/TraduccionManager.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Crud;
+using Entities_POJO;
+using Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraductorFaz
+{
+    public class TraduccionManager
+    {
+        private TraduccionCrudFactory crudTraduccion;
+
+        public TraduccionManager()
+        {
+            crudTraduccion = new TraduccionCrudFactory();
+        }
+
+        public void Create(Traduccion traduccion)
+        {
+            try
+            {
+            var c = crudTraduccion.Retrieve<Traduccion>(traduccion);
+
+            if (c != null)
+            {
+                //Traduccion already exists.
+                throw new BussinessException(3);
+            }
+            crudTraduccion.Create(traduccion);
+        }
+        catch (Exception ex)
+        {
+            ExceptionManager.GetInstance().Process(ex);
+            }
+    }
+
+        public List<Traduccion> RetrieveAll()
+    {
+        return crudTraduccion.RetrieveAll<Traduccion>();
+    }
+
+    public Traduccion RetrieveById(Traduccion traduccion)
+    {
+        return crudTraduccion.Retrieve<Traduccion>(traduccion);
+    }
+
+    internal void Update(Traduccion traduccion)
+    {
+        crudTraduccion.Update(traduccion);
+    }
+
+    internal void Delete(Traduccion traduccion)
+    {
+            crudTraduccion.Delete(traduccion);
+    }
+    }
+}
