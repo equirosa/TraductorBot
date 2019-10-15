0e38bbf3f795d3e3fa0a8fd434d222730d4c3859
equirosa <eduardo@eduardoquiros.com>
2019-10-15 10:17:22 -0600

    Create translation class.

---
+++ TraductorFaz/Entities-POJO/Translation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_POJO
+{
+    public class Translation : BaseEntity
+    {
+        private string Spanish { get; set; }
+        private Language Language { get; set; }
+        private string Translated { get; set; }
+
+    }
+}
